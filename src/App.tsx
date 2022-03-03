5aaccab3824b991f0347b0df3ce8b1756445146b
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-03-03 03:27:56 -0300

    :sparkles: implement project code with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+interface Empregado {
+  id: number;
+  nome: string;
+  salario: number;
+  funcionario: boolean;
+  departamento?: string;
+}
+
 function App() {
+  const pessoas: Empregado[] = [
+    {
+      id: 1,
+      nome: "Steve",
+      salario: 1000,
+      funcionario: true,
+      departamento: "TI",
+    },
+    {
+      id: 2,
+      nome: "Maria",
+      salario: 0,
+      funcionario: false,
+    },
+    {
+      id: 3,
+      nome: "Fernanda",
+      salario: 1200,
+      funcionario: true,
+      departamento: "RH",
+    },
+    {
+      id: 4,
+      nome: "Carlos",
+      salario: 0,
+      funcionario: false,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pessoas.map(pessoa => <div>
+        <p>{pessoa.id} - <strong>{pessoa.nome}</strong> - Salário de R$ {pessoa.salario} - {pessoa.funcionario ? 'É funcionário' : 'Não é funcionário'} {pessoa.funcionario && pessoa.departamento && `do departamento de ${pessoa.departamento}`}</p>
+      </div>)}
     </div>
   );
 }
